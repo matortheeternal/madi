1fce7c5107e3f9bb59565e51e4dacd45188da9e2
Mator <why101@msn.com>
2016-03-10 11:46:59 -0800

    create merge reports table migration updated with active record column attributes

--- db/migrate/20160310190907_create_merge_reports.rb
+++ db/migrate/20160310190907_create_merge_reports.rb
@@ -1,15 +1,15 @@
 class CreateMergeReports < ActiveRecord::Migration
   def change
     create_table :merge_reports do |t|
-      t.string :submitted_by
-      t.boolean :approved
-      t.string :game
-      t.string :filename
-      t.string :hash
-      t.integer :record_count
-      t.integer :rating
-      t.string :merge_version
-      t.string :notes
+      t.string :submitted_by, :limit => 32, :null => false
+      t.boolean :approved, :default => false
+      t.string :game, :limit => 32, :null => false
+      t.string :filename, :limit => 64, :null => false
+      t.string :hash, :limit => 16, :null => false
+      t.integer :record_count, :null => false
+      t.integer :rating, :limit => 1, :null => false
+      t.string :merge_version, :limit => 16, :null => false
+      t.string :notes, :limit => 255
       t.timestamp :approved_at
 
       t.timestamps null: false

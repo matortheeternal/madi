55da700ef7c9560fe6abcec8714930dcfa0c47c2
Mator <why101@msn.com>
2016-03-10 12:50:08 -0800

    added rack-attack middleware to keep the application secure! :lock:

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Mati
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # use rack-attack for middleware
+    config.middleware.use Rack::Attack
   end
 end

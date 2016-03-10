4a737473e77eb8bdc9e9746b9256562f1a7a06d6
Mator <why101@msn.com>
2016-03-10 11:18:07 -0800

    create users migration updated with data limits

--- db/migrate/20160310190820_create_users.rb
+++ db/migrate/20160310190820_create_users.rb
@@ -1,13 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :role
-      t.datetime :first_seen
-      t.datetime :last_seen
-      t.integer :times_seen
-      t.integer :download
-      t.integer :upload
+      t.string :username, :limit => 32, :null => false
+      t.string :role, :limit => 16, :default => :user
+      t.integer :times_seen, :default => 1
+      t.integer :download, :limit => 8
+      t.integer :upload, :limit => 8
 
       t.timestamps null: false
     end

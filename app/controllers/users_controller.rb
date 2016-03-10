75fb7d854fa1ff071b0b733079e237951f38c532
Mator <why101@msn.com>
2016-03-10 15:59:33 -0800

    users controller updated to support json and use cancan for authorization.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /users
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @users}
+    end
   end
 
   # GET /users/1
   def show
+    respond_to do |format|
+      format.html
+      format.json { render :json => @user}
+    end
   end
 
   # GET /users/new
@@ -23,36 +32,43 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @user.update(game_params)
+        format.html { redirect_to @user, notice: 'Game was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /users/1
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :role, :times_seen, :download, :upload)
+      authorize! :assign_roles, @user if params[:user][:role]
+      params.require(:user).permit(:username, :email, :role)
     end
 end

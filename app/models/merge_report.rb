f1849f9cb6fcf80beb2ea619b989f0364975e89e
Mator <why101@msn.com>
2016-06-22 16:08:46 -0700

    added filtering scopes to MergeReport

--- app/models/merge_report.rb
+++ app/models/merge_report.rb
@@ -1,4 +1,17 @@
 class MergeReport < ActiveRecord::Base
+  include Sortable, Filterable
+
+  # BOOLEAN FILTERS
+  scope :approved, -> (bool) { where(approved: true) if bool }
+  # GENERAL FILTERS
+  scope :game, -> (game) { where(game: game) }
+  scope :search, -> (search) { where("filename LIKE ?", "%#{search}%") }
+  scope :record_count, -> (range) { where(record_count: range[:min]..range[:max]) }
+  scope :rating, -> (range) { where(rating: range[min]..range[max])}
+  scope :version, -> (version) { where(merge_version: version) }
+  scope :created, -> (range) { where(created_at: parseDate(range[:min])..parseDate(range[:max])) }
+  scope :updated, -> (range) { where(updated_at: parseDate(range[:min])..parseDate(range[:max])) }
+
   belongs_to :user, :inverse_of => 'merge_reports'
 
   def as_json(options={})

6ec73c15b31fe5f067bbded1907baec9e5407b55
Mator <why101@msn.com>
2016-06-22 15:53:58 -0700

    added post routes for filtering for all the things

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # User submitted content
   resources :smash_reports
+  match '/smash_reports/index', to: 'smash_reports#index', via: [:post]
+
   resources :merge_reports
+  match '/merge_reports/index', to: 'merge_reports#index', via: [:post]
+
+  match '/smash_settings/index', to: 'smash_settings#index', via: [:post]
   resources :smash_settings
-  resources :mator_smash_statistics
-  resources :merge_plugins_statistics
-  resources :blacklist_entries
+
+  # Users and Statistics
   resources :users
+  match '/users/index', to: 'users#index', via: [:post]
+
+  resources :mator_smash_statistics
+  match '/mator_smash_statistics/index', to: 'mator_smash_statistics#index', via: [:post]
+
+  resources :merge_plugins_statistics
+  match '/merge_plugins_statistics/index', to: 'merge_plugins_statistics#index', via: [:post]
+
+  # Blacklist Entries
+  resources :blacklist_entries
+  match '/blacklist_entries/index', to: 'blacklist_entries#index', via: [:post]
   
   # welcome page
   resources 'welcome', only: [:index]

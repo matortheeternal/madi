508632f644e48285bcaacfd3aa0177fda34e7c00
Mator <why101@msn.com>
2016-06-22 15:07:52 -0700

    added as_json defaults to merge_reports, smash_reports, smash_settings, and users

--- app/models/smash_report.rb
+++ app/models/smash_report.rb
@@ -1,3 +1,19 @@
 class SmashReport < ActiveRecord::Base
   belongs_to :user, :inverse_of => 'smash_reports'
+
+  def as_json(options={})
+    if JsonHelpers.json_options_empty(options)
+      default_options = {
+          :except => [:id, :user_id],
+          :include => {
+              :user => {
+                  :only => [:username]
+              }
+          }
+      }
+      super(options.merge(default_options))
+    else
+      super(options)
+    end
+  end
 end
--- app/models/smash_setting.rb
+++ app/models/smash_setting.rb
@@ -1,3 +1,19 @@
 class SmashSetting < ActiveRecord::Base
   belongs_to :user, :inverse_of => 'smash_settings'
+
+  def as_json(options={})
+    if JsonHelpers.json_options_empty(options)
+      default_options = {
+          :except => [:id, :user_id],
+          :include => {
+              :user => {
+                  :only => [:username]
+              }
+          }
+      }
+      super(options.merge(default_options))
+    else
+      super(options)
+    end
+  end
 end

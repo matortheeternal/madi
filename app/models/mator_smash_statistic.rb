f6727d74b80b89a8971b8fbd4e75170a154a800b
Mator <why101@msn.com>
2016-06-22 16:25:59 -0700

    added filterable and sortable modules to mator_smash_static, merge_plugins_statistic, and user models

--- app/models/mator_smash_statistic.rb
+++ app/models/mator_smash_statistic.rb
@@ -1,3 +1,5 @@
 class MatorSmashStatistic < ActiveRecord::Base
+  include Sortable, Filterable
+
   belongs_to :user, :inverse_of => 'mator_smash_statistic'
 end
--- app/models/merge_plugins_statistic.rb
+++ app/models/merge_plugins_statistic.rb
@@ -1,3 +1,5 @@
 class MergePluginsStatistic < ActiveRecord::Base
+  include Sortable, Filterable
+
   belongs_to :user, :inverse_of => 'merge_plugins_statistic'
 end

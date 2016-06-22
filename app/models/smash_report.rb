8efd5192211202780ae812e4c6671004d61f2c82
Mator <why101@msn.com>
2016-06-22 16:10:39 -0700

    added filtering scopes to smash_report model

--- app/models/smash_report.rb
+++ app/models/smash_report.rb
@@ -1,4 +1,16 @@
 class SmashReport < ActiveRecord::Base
+  include Sortable, Filterable
+
+  # BOOLEAN FILTERS
+  scope :approved, -> (bool) { where(approved: true) if bool }
+  # GENERAL FILTERS
+  scope :game, -> (game) { where(game: game) }
+  scope :search, -> (search) { where("filename LIKE ?", "%#{search}%") }
+  scope :record_count, -> (range) { where(record_count: range[:min]..range[:max]) }
+  scope :version, -> (version) { where(smash_version: version) }
+  scope :created, -> (range) { where(created_at: parseDate(range[:min])..parseDate(range[:max])) }
+  scope :updated, -> (range) { where(updated_at: parseDate(range[:min])..parseDate(range[:max])) }
+
   belongs_to :user, :inverse_of => 'smash_reports'
   belongs_to :smash_setting, :inverse_of => 'smash_reports'
 

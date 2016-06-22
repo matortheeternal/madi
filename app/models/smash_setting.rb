0688b1abf034c768d6bea5d13f26984a657ede8f
Mator <why101@msn.com>
2016-06-22 15:52:41 -0700

    added smash setting scopes for filtering, also added sortable and filterable modules

--- app/models/smash_setting.rb
+++ app/models/smash_setting.rb
@@ -1,4 +1,16 @@
 class SmashSetting < ActiveRecord::Base
+  include Sortable, Filterable
+
+  # BOOLEAN FILTERS
+  scope :approved, -> (bool) { where(approved: true) if bool }
+  # GENERAL FILTERS
+  scope :game, -> (game) { where(game: game) }
+  scope :search, -> (search) { where("name LIKE ?", "%#{search}%") }
+  scope :records, -> (records) { where("records LIKE ?", "%#{records}%") }
+  scope :created, -> (range) { where(created_at: parseDate(range[:min])..parseDate(range[:max])) }
+  scope :updated, -> (range) { where(updated_at: parseDate(range[:min])..parseDate(range[:max])) }
+
+
   belongs_to :user, :inverse_of => 'smash_settings'
 
   def as_json(options={})

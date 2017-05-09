2ca1ce1c22895e07ee4175b0a64a07c02862df82
Mator <why101@msn.com>
2017-05-08 22:18:02 -0700

    fleshed out rack attack throttles

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,77 +1,56 @@
 class Rack::Attack
-
-  ### Configure Cache ###
-
-  # If you don't want to use Rails.cache (Rack::Attack's default), then
-  # configure it here.
-  #
-  # Note: The store is only used for throttling (not blacklisting and
-  # whitelisting). It must implement .increment and .write like
-  # ActiveSupport::Cache::Store
-
-  # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
-
-  ### Throttle Spammy Clients ###
-
-  # If any single client IP is making tons of requests, then they're
-  # probably malicious or a poorly-configured scraper. Either way, they
-  # don't deserve to hog all of the app server's CPU. Cut them off!
-  #
-  # Note: If you're serving assets through rack, those requests may be
-  # counted by rack-attack and this throttle may be activated too
-  # quickly. If so, enable the condition to exclude them from tracking.
-
-  # Throttle all requests by IP (60rpm)
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
-    req.ip # unless req.path.start_with?('/assets')
+  # Throttle all requests by IP (180rpm) excluding assets
+  throttle('req/ip', :limit => 900, :period => 5.minutes) do |req|
+    req.ip unless req.path.start_with?('/assets')
   end
 
-  ### Prevent Brute-Force Login Attacks ###
-
-  # The most common brute-force login attack is a brute-force password
-  # attack where an attacker simply tries a large number of emails and
-  # passwords to see if any credentials match.
-  #
-  # Another common method of attack is to use a swarm of computers with
-  # different IPs to try brute-forcing a password for a specific account.
-
   # Throttle POST requests to /login by IP address
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.ip}"
-  throttle('logins/ip', :limit => 5, :period => 20.seconds) do |req|
+  throttle('logins/ip', :limit => 5, :period => 10.seconds) do |req|
     if req.path == '/users/sign_in' && req.post?
       req.ip
     end
   end
 
   # Throttle POST requests to /login by email param
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/email:#{req.email}"
-  #
-  # Note: This creates a problem where a malicious user could intentionally
-  # throttle logins for another user and force their login requests to be
-  # denied, but that's not very common and shouldn't happen to you. (Knock
-  # on wood!)
-  throttle("logins/email", :limit => 5, :period => 20.seconds) do |req|
+  throttle("sign_in/account", :limit => 5, :period => 10.seconds) do |req|
     if req.path == '/users/sign_in' && req.post?
       # return the email if present, nil otherwise
-      req.params['email'].presence
+      req.params['login'].presence
+    end
+  end
+
+  # Throttle POST requests to new content by IP
+  throttle("new_content", :limit => 60, :period => 3.minutes) do |req|
+    if req.post? &&
+        (req.path == '/merge_reports' ||
+            req.path == '/smash_reports' ||
+            req.path == '/smash_settings')
+      req.ip
+    end
+  end
+
+  # Throttle dictionary requests
+  throttle("dictionaries", :limit => 5, :period => 5.minutes) do |req|
+    if req.path.end_with?('dictionary.txt')
+      req.ip
     end
   end
 
   ### Custom Throttle Response ###
+  self.throttled_response = lambda do |env|
+    # respond 304 not modified for throttles where we want to force caching
+    # if env['rack.attack.matched'] == 'tags_index'
+    #   status = 304
+    # respond 503 service unavailable if it looks like a DDOS attack
+    if env['rack.attack.matched'] == 'req/ip'
+      status = 503
+    else
+      status = 429
+    end
 
-  # By default, Rack::Attack returns an HTTP 429 for throttled responses,
-  # which is just fine.
-  #
-  # If you want to return 503 so that the attacker might be fooled into
-  # believing that they've successfully broken your app (or you just want to
-  # customize the response), then uncomment these lines.
-  # self.throttled_response = lambda do |env|
-  #  [ 503,  # status
-  #    {},   # headers
-  #    ['']] # body
-  # end
+    # response
+    [ status,  # status
+        {},   # headers
+        ['']] # body
+  end
 end
